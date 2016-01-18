475d93f9abc0b58f1eb7a451e3fc230e3cbf9150
agallego <gallego.alexx@gmail.com>
2016-01-18 13:12:51 -0500

    crashing seastar project

--- src/smf/main.cc
+++ src/smf/main.cc
@@ -1,4 +1,20 @@
+#include "core/app-template.hh"
+#include "core/reactor.hh"
 #include <iostream>
-ing main(int argc, char **argv, char** env){
-  std::cout << "hello world" << std::endl;
+#include <stdexcept>
+int main(int argc, char **argv) {
+  app_template app;
+  try {
+    app.run(argc, argv, [] {
+      std::cout << "hello world\n";
+      return make_ready_future<>();
+    });
+  } catch(const std::runtime_error &e) {
+    std::cerr << "Couldn't start application: " << e.what() << "\n";
+    return 1;
+  } catch(const std::exception &e) {
+    std::cerr << "wtf: " << e.what() << "\n";
+    return 1;
+  }
+  return 0;
 }
