0ff2663621ea3dea5c6f7d3f7a3180650fb594d1
agallego <gallego.alexx@gmail.com>
2016-01-19 20:24:22 -0500

    bash script while

--- travis_deps.sh
+++ travis_deps.sh
@@ -8,7 +8,7 @@ cd $git_root/meta
 source source_ansible_bash
 ansible-playbook playbooks/devbox_all.yml &
 ansible_pid=$!
-while ps -p $ansible_pid;
+while ps -p $ansible_pid; do
       echo "DUMB ASS TRAVIS";
       sleep 5;
 done
