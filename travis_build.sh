d1008e793cd659ef4951d53cb1b5ccb5806ca53f
agallego <gallego.alexx@gmail.com>
2016-01-20 09:45:43 -0500

    Remove the set -ex at the begining of script

--- travis_build.sh
+++ travis_build.sh
@@ -1,9 +1,8 @@
 #!/bin/bash --login
-set -ex
+#set -ex
 lsb_release -a
 git_root=$(git rev-parse --show-toplevel)
 nprocs=$(grep -c ^processor /proc/cpuinfo)
-
 mkdir -p $git_root/build
 cd $git_root/build
 cmake $git_root
