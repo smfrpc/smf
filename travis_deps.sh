538bdd7948508c5eea86490918614d81bccc3c98
agallego <gallego.alexx@gmail.com>
2016-01-19 20:21:57 -0500

    fix travis

--- travis_deps.sh
+++ travis_deps.sh
@@ -4,5 +4,11 @@ lsb_release -a
 git_root=$(git rev-parse --show-toplevel)
 
 cd $git_root/meta
+
 source source_ansible_bash
-ansible-playbook playbooks/devbox_all.yml
+ansible-playbook playbooks/devbox_all.yml &
+ansible_pid=$!
+while ps -p $ansible_pid;
+      echo "DUMB ASS TRAVIS";
+      sleep 5;
+done
