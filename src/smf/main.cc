109a8f216860cb4bc6f45969c680e3c8bb8c738b
agallego <gallego.alexx@gmail.com>
2016-02-19 17:58:16 -0500

    Adding folly to build

--- src/smf/main.cc
+++ src/smf/main.cc
@@ -1,20 +1,10 @@
-#include "core/app-template.hh"
-#include "core/reactor.hh"
 #include <iostream>
-#include <stdexcept>
+
+#include "folly/SocketAddress.h"
+
 int main(int argc, char **argv) {
-  app_template app;
-  try {
-    app.run(argc, argv, [] {
-      std::cout << "hello world\n";
-      return make_ready_future<>();
-    });
-  } catch(const std::runtime_error &e) {
-    std::cerr << "Couldn't start application: " << e.what() << "\n";
-    return 1;
-  } catch(const std::exception &e) {
-    std::cerr << "wtf: " << e.what() << "\n";
-    return 1;
-  }
+  folly::SocketAddress a;
+  a.setFromHostPort("localhost", 2344);
+  std::cout << "Address is: " << a.describe() << std::endl;
   return 0;
 }
