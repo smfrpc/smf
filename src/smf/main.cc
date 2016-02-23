f0b7ee6343fe18cc346d9964fe36041deb0cdb85
agallego <gallego.alexx@gmail.com>
2016-02-23 13:53:12 -0500

    Adding proxygen example

--- src/smf/main.cc
+++ src/smf/main.cc
@@ -1,9 +1,79 @@
-#include <iostream>
-#include "folly/SocketAddress.h"
+#include <gflags/gflags.h>
+#include <folly/Memory.h>
+#include <folly/Portability.h>
+#include <folly/io/async/EventBaseManager.h>
+#include <proxygen/httpserver/HTTPServer.h>
+#include <proxygen/httpserver/RequestHandlerFactory.h>
+#include <unistd.h>
 
-int main(int argc, char **argv) {
-  folly::SocketAddress a;
-  a.setFromHostPort("localhost", 2344);
-  if(true)
-    std::cout << "Test folly: Address is: " << a.describe() << std::endl;
+#include "EchoHandler.h"
+#include "EchoStats.h"
+
+using namespace EchoService;
+using namespace proxygen;
+
+using folly::EventBase;
+using folly::EventBaseManager;
+using folly::SocketAddress;
+
+using Protocol = HTTPServer::Protocol;
+
+DEFINE_int32(http_port, 11000, "Port to listen on with HTTP protocol");
+DEFINE_int32(spdy_port, 11001, "Port to listen on with SPDY protocol");
+DEFINE_string(ip, "localhost", "IP/Hostname to bind to");
+DEFINE_int32(threads, 0, "Number of threads to listen on. Numbers <= 0 "
+             "will use the number of cores on this machine.");
+
+class EchoHandlerFactory : public RequestHandlerFactory {
+ public:
+  void onServerStart() noexcept override {
+    stats_.reset(new EchoStats);
+  }
+
+  void onServerStop() noexcept override {
+    stats_.reset();
+  }
+
+  RequestHandler* onRequest(RequestHandler*, HTTPMessage*) noexcept override {
+    return new EchoHandler(stats_.get());
+  }
+
+ private:
+  folly::ThreadLocalPtr<EchoStats> stats_;
+};
+
+int main(int argc, char* argv[]) {
+  gflags::ParseCommandLineFlags(&argc, &argv, true);
+  google::InitGoogleLogging(argv[0]);
+  google::InstallFailureSignalHandler();
+
+  std::vector<HTTPServer::IPConfig> IPs = {
+    {SocketAddress(FLAGS_ip, FLAGS_http_port, true), Protocol::HTTP},
+    {SocketAddress(FLAGS_ip, FLAGS_spdy_port, true), Protocol::SPDY},
+  };
+
+  if (FLAGS_threads <= 0) {
+    FLAGS_threads = sysconf(_SC_NPROCESSORS_ONLN);
+    CHECK(FLAGS_threads > 0);
+  }
+
+  HTTPServerOptions options;
+  options.threads = static_cast<size_t>(FLAGS_threads);
+  options.idleTimeout = std::chrono::milliseconds(60000);
+  options.shutdownOn = {SIGINT, SIGTERM};
+  options.enableContentCompression = true;
+  options.handlerFactories = RequestHandlerChain()
+      .addThen<EchoHandlerFactory>()
+      .build();
+
+  HTTPServer server(std::move(options));
+  server.bind(IPs);
+
+  // Start HTTPServer mainloop in a separate thread
+  std::thread t([&] () {
+    server.start();
+  });
+
+  t.join();
+  return 0;
 }
