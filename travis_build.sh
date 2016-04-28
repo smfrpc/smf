c5a5f77a4588a9fda4cf246f033224d831be960f
agallego <gallego.alexx@gmail.com>
2016-04-27 21:41:24 -0400

    source compile from travis

--- travis_build.sh
+++ travis_build.sh
@@ -2,9 +2,4 @@
 #set -ex
 lsb_release -a
 git_root=$(git rev-parse --show-toplevel)
-nprocs=$(grep -c ^processor /proc/cpuinfo)
-mkdir -p $git_root/build
-cd $git_root/build
-cmake $git_root
-make -j$nprocs
-ctest -j$nprocs -V
+exec $git_root/compile test
