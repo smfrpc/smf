14c197ebc6ff0da15cab13e3dcc0c121e40065fb
agallego <gallego.alexx@gmail.com>
2016-05-21 10:10:47 -0400

    need to add rcp_stats

---
+++ src/rpc/rpc_stats.h
@@ -0,0 +1,25 @@
+#pragma once
+
+struct rpc_stats {
+  size_t get_requests{};
+};
+
+    // size_t _get_hits {};
+    // size_t _get_misses {};
+    // size_t _set_adds {};
+    // size_t _set_replaces {};
+    // size_t _cas_hits {};
+    // size_t _cas_misses {};
+    // size_t _cas_badval {};
+    // size_t _delete_misses {};
+    // size_t _delete_hits {};
+    // size_t _incr_misses {};
+    // size_t _incr_hits {};
+    // size_t _decr_misses {};
+    // size_t _decr_hits {};
+    // size_t _expired {};
+    // size_t _evicted {};
+    // size_t _bytes {};
+    // size_t _resize_failure {};
+    // size_t _size {};
+    // size_t _reclaims{};
