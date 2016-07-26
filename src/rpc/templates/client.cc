525f77a01c2d8ba2a5c6e0f1939309b5f72d4442
Alexander Gallego <gallego.alexx@gmail.com>
2016-07-26 11:50:07 -0400

    rcp:template/client.cc: configurable num of reqs.

--- src/rpc/templates/client.cc
+++ src/rpc/templates/client.cc
@@ -22,15 +22,14 @@ class rpc_client_wrapper {
     client_ = make_lw_shared<smf_gen::fbs::rpc::SmurfStorageClient>(
       ipv4_addr{ip, port});
     client_->enable_histogram_metrics();
+    auto req = smf_gen::fbs::rpc::CreateRequest(builder_,
+                                                builder_.CreateString("hello"));
+    builder_.Finish(req);
   }
 
   future<> send_request() {
-    auto req = smf_gen::fbs::rpc::CreateRequest(
-      builder_, builder_.CreateString("hello from client"));
-    builder_.Finish(req);
     smf::rpc_envelope env(builder_.GetBufferPointer(), builder_.GetSize());
-
-    builder_.Clear(); // reuse it
+    // builder_.Clear(); // reuse it
     return client_->GetSend(std::move(env))
       .then([](auto reply) mutable {
         if(!reply) {
@@ -39,12 +38,12 @@ class rpc_client_wrapper {
       })
       .then([this]() mutable {
         if(this->num_of_req_-- > 0) {
-          smf::LOG_INFO("Sending req: {}", num_of_req_);
           return this->send_request();
         } else {
           smf::LOG_INFO("================================");
           smf::LOG_INFO("Thread ID: {}", std::this_thread::get_id());
-          sstring s = "histogram_client_shard_" + to_sstring(engine().cpu_id()) + ".txt";
+          sstring s =
+            "histogram_client_shard_" + to_sstring(engine().cpu_id()) + ".txt";
           FILE *fp = fopen(s.c_str(), "w");
           if(fp) {
             client_->get_histogram()->print(fp);
@@ -68,18 +67,20 @@ int main(int args, char **argv, char **env) {
   std::cerr << "About to start the client" << std::endl;
   distributed<rpc_client_wrapper> clients;
   app_template app;
-  app.add_options()("rpc_port", bpo::value<uint16_t>()->default_value(11225),
-                    "rpc port")(
+  app.add_options()("req_num", bpo::value<size_t>()->default_value(400),
+                    "number of requests")(
+    "rpc_port", bpo::value<uint16_t>()->default_value(11225), "rpc port")(
     "ip_address", bpo::value<std::string>()->default_value("127.0.0.1"),
     "ip address of server");
   try {
     return app.run_deprecated(args, argv, [&] {
       auto &&config = app.configuration();
       uint16_t port = config["rpc_port"].as<uint16_t>();
+      size_t req_num = config["req_num"].as<size_t>();
       const char *ip = config["ip_address"].as<std::string>().c_str();
       smf::LOG_INFO("setting up exit hooks");
       engine().at_exit([&] { return clients.stop(); });
-      return clients.start(ip, port, 400)
+      return clients.start(ip, port, req_num)
         .then([&clients] {
           smf::LOG_INFO("Creating connections");
           return clients.invoke_on_all(&rpc_client_wrapper::connect);
