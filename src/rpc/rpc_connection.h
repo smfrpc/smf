1f5230721ab1235501214d89d7ca9ca861406421
Alexander Gallego <gallego.alexx@gmail.com>
2016-07-28 20:37:39 -0400

    rpc::conection make conn invalid without error
    * the connection could be invalid in the application sense. I.e.:
      we should not accept any connections, we should just flush and move on
      and try to exit early out, but wait to flush

--- src/rpc/rpc_connection.h
+++ src/rpc/rpc_connection.h
@@ -5,7 +5,8 @@
 
 namespace smf {
 
-struct rpc_connection {
+class rpc_connection {
+  public:
   rpc_connection(connected_socket fd)
     : socket(std::move(fd))
     , istream(socket.input())
@@ -14,12 +15,14 @@ struct rpc_connection {
   input_stream<char> istream;
   output_stream<char> ostream;
 
-
-  bool is_valid() { return !istream.eof() && !has_error(); }
+  void set_invalid() { valid_ = false; }
+  bool is_valid() { return !istream.eof() && !has_error() && valid_; }
   bool has_error() const { return error_.operator bool(); }
   void set_error(const char *e) { error_ = sstring(e); }
   sstring get_error() const { return error_.value(); }
-  std::experimental::optional<sstring> error_;
 
+  private:
+  std::experimental::optional<sstring> error_;
+  bool valid_{true};
 };
 }
