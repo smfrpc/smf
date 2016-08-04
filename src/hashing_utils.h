efc1ea9c793f7c2a72b698cb3fb653b1ba94ac0a
Alexander Gallego <gallego.alexx@gmail.com>
2016-08-04 17:19:18 -0400

    trimming for the last 32bits of to use xxhash64
    * use xxhash64 for throughput and trim the top32 bits

--- src/hashing_utils.h
+++ src/hashing_utils.h
@@ -8,7 +8,10 @@ inline uint32_t crc_32(const char *data, const size_t &length) {
   return result.checksum();
 }
 inline uint32_t xxhash(const char *data, const size_t &length) {
-  uint64_t t = XXH64(data, length, 0);
+  constexpr static uint64_t kTrim =
+    uint64_t(0) | std::numeric_limits<uint32_t>::max();
+  // clean up the top bits after the hash
+  uint64_t t = XXH64(data, length, 0) & kTrim;
   return static_cast<uint32_t>(t);
 }
 }
