35ecfbfac2bebb8d2a401711cec1ee1af50d0a8c
Alexander Gallego <gallego.alexx@gmail.com>
2017-03-14 22:08:00 -0400

    wip: test LOLL

--- src/chain_replication/chain_replication_service.cc
+++ src/chain_replication/chain_replication_service.cc
@@ -13,14 +13,19 @@ namespace smf {
 namespace chains {
 future<smf::rpc_envelope> chain_replication_service::put(
   smf::rpc_recv_typed_context<tx_put_request> &&record) {
-  unsigned core = put_to_lcore(record.get());
-  return smp::submit_to(core, [this, record = std::move(record)]() mutable {
-           // do the actual put on the correct core
-           return do_put(std::move(record)).then([](auto ctx) {
-             auto ctx_p = make_lw_shared<smf::rpc_envelope>(std::move(ctx));
-             return make_ready_future<lw_shared_ptr<smf::rpc_envelope>>(ctx_p);
-           });
-         })
+  // TODO(agallego) - this is lame.
+  // seastar copies the returned value on a submit_to
+  // so we have to have a type that does a copy
+  //
+  return smp::submit_to(put_to_lcore(record.get()),
+                        [this, record = std::move(record)]() mutable {
+                          return do_put(std::move(record)).then([](auto ctx) {
+                            using type = lw_shared_ptr<smf::rpc_envelope>;
+                            type ctx_p =
+                              make_lw_shared<smf::rpc_envelope>(std::move(ctx));
+                            return make_ready_future<type>(ctx_p);
+                          });
+                        })
     .then([](auto ctx_ptr) {
       return make_ready_future<smf::rpc_envelope>(std::move(*ctx_ptr));
     });
