3f29cdd711c4d092c776f6b557df28829e87ff1e
Alexander Gallego <gallego.alexx@gmail.com>
2017-06-29 22:03:38 -0400

    chain_replication: add better hasher from request to lcore map
    before we would ruin the xxhash performance/distribution by simply
    xor'ing the hash of the topic w/ the partition. Using
    fmt::MemoryWriter allows us to get better spread from put to lcore map
    
    * use lw_shared so that concurrent coroutines can close filehandle
    
      Currently we use a unique_ptr which may or may not be alive when
      trying to close the file handles. To prevent that we give ownership
      of the filehandle to a lw_shared ptr so a background co-routine can
      properly cleanup w/out segfaults
    
    * defer the hashing to a templated function
    
    * Adding integration tests for chain replication utils

--- src/chain_replication/chain_replication_utils.cc
+++ src/chain_replication/chain_replication_utils.cc
@@ -1,22 +1,30 @@
 // Copyright (c) 2016 Alexander Gallego. All rights reserved.
 //
 #include <limits>
+#include <type_traits>
+
+#include <fmt/format.h>
 
 #include "chain_replication/chain_replication_utils.h"
 #include "hashing/hashing_utils.h"
 
 namespace smf {
 namespace chains {
-/** \brief map the request to the lcore that is going to handle the put */
-uint32_t put_to_lcore(const tx_put_request *p) {
-  const uint64_t partition = static_cast<uint64_t>(p->partition());
-  const uint64_t topic_hash =
-    xxhash_64(p->topic()->c_str(), p->topic()->size());
 
-  const uint64_t tp_hash = topic_hash ^ partition;
-
+template <typename T>
+requires(std::is_pointer<T>::value == true) uint32_t lcore_hash(T p) {
+  fmt::MemoryWriter w;
+  w.write("{}:{}", p->topic()->c_str(), p->partition());
+  const uint64_t tp_hash = xxhash_64(w.data(), w.size());
   return jump_consistent_hash(tp_hash, seastar::smp::count);
 }
+
+/** \brief map the request to the lcore that is going to handle the put */
+uint32_t put_to_lcore(const tx_put_request *p) { return lcore_hash(p); }
+
+/** \brief map the request to the lcore that is going to handle the put */
+uint32_t get_to_lcore(const tx_get_request *p) { return lcore_hash(p); }
+
 
 }  // end namespace chains
 }  // end namespace smf
