0b58b61fce2524f5e9dda30bb173a812308d49eb
Alexander Gallego <gallego.alexx@gmail.com>
2017-07-30 00:00:22 -0400

    Adding cppcheck and cpplint to remote repos
    Download and run cppcheck, also run cpplint
    from their respective upstream parents at specific checkins

--- misc/format_files.sh
+++ misc/format_files.sh
@@ -1,22 +1,55 @@
-#!/bin/bash --login
+#!/bin/bash
+
+set -e
+
 CURRENT=${PWD}
 git_root=$(git rev-parse --show-toplevel)
-linter="${git_root}/misc/cpplint.py --counting=detailed"
-git_user_name=$(git config user.name)
-cd ${git_root}
-FILES=$(git ls-files | grep -iP "\.(hpp|cc|h|java|scala)$")
-for f in ${FILES}; do
-    if [[ $(head -n1 ${f} | grep Copyright) == "" ]]; then
-        f_contents=$(cat ${f});
-        echo "// Copyright 2017 ${git_user_name}" > ${f};
-        echo "//" >> ${f}
-        echo "${f_contents}" >> ${f};
-    fi
-    # echo "Formatting ${f}";
-    clang-format -i ${f};
-    # echo "cpplinting ${f}";
-    # uses CPPLINT.cfg
-    python ${linter} ${git_root}/${f}
 
+cpp_lint_cmd=$git_root/src/third_party/bin/cpplint.py
+cppcheck_cmd=$git_root/src/third_party/bin/cppcheck
+clang_format_cmd=$(which clang-format)
+git_user_name=$(git config user.name)
+seastar_includes=$(pkg-config --cflags-only-I $git_root/meta/tmp/seastar/build/debug/seastar.pc)
+
+
+pushd ${git_root}
+FILES=$(git ls-files | grep -iP "\.(hpp|cc|h|java|proto)$")
+
+for f in ${FILES}; do
+    file=$git_root/$f
+    echo $file
+    # Check copyright
+    #
+    if [[ $(head -n1 ${f} | grep Copyright) == "" ]]; then
+        f_contents=$(cat ${file});
+        echo "// Copyright 2017 ${git_user_name}" > ${f};
+        echo "//" >> ${file}
+        echo "${f_contents}" >> ${file};
+    fi
+
+    #echo "clang-format ${file}";
+    $clang_format_cmd -i ${file};
+
+    #echo "cppcheck ${file}";
+    $cppcheck_cmd \
+        -DUCHAR_MAX=255 \
+        -I $git_root/src \
+        -I $git_root/src/third_party/include \
+        -I $git_root \
+        ${seastar_includes} \
+        -I /usr/local/include \
+        --enable=warning,performance,portability,missingInclude \
+        --std=c++11 \
+        --language=c++ \
+        --quiet \
+        --suppress=missingIncludeSystem --suppress=preprocessorErrorDirective \
+        --template="[{severity}][{id}]{message} {callstack} {file}:{line}" \
+        $file
+
+    #echo "CPPLINT ${file}"
+    $cpp_lint_cmd --verbose=5 --counting=detailed ${file} > /dev/null
 done
-cd ${CURRENT}
+
+
+
+popd
--- src/rpc/futurize_utils.h
+++ src/rpc/futurize_utils.h
@@ -1,13 +1,15 @@
+// Copyright 2017 Alexander Gallego
+//
 #pragma once
 
 namespace smf {
 
-  /// \brief used in almost all RPC applications like
-  /// \code
-  ///    if (!record) {
-  ///       return futurize_status_for_type<smf::chains::tx_put_reply>(400);
-  ///    }
-  /// \endcode
+/// \brief used in almost all RPC applications like
+/// \code
+///    if (!record) {
+///       return futurize_status_for_type<smf::chains::tx_put_reply>(400);
+///    }
+/// \endcode
 template <typename T> auto futurize_status_for_type(uint32_t status) {
   using t = smf::rpc_typed_envelope<T>;
   t data;
