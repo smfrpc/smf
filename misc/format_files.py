4e93f0a902ab3ad57e386467076a92b9f4f59fbe
Alexander Gallego <gallego.alexx@gmail.com>
2017-07-30 18:34:42 -0400

    wip2

--- misc/format_files.py
+++ misc/format_files.py
@@ -32,7 +32,7 @@ def get_git_user():
 
 
 def run_subprocess(cmd):
-    logger.info("Executing command: %s" % cmd)
+    logger.debug("Executing command: %s" % cmd)
     proc = subprocess.Popen(
         "exec %s" % cmd, stdout=sys.stdout, stderr=sys.stderr, shell=True)
     return_code = 0
@@ -83,7 +83,8 @@ def is_double_slash(filename):
 
 
 def get_legal_header(filename):
-    hdr = "%s Copyright %s %s\n%s"
+    hdr = "%s Copyright %s %s\n%s\n\n"
+
     def comment_char():
         if is_double_slash(filename): return "//"
         return "#"
@@ -100,7 +101,7 @@ def is_script_file(filename):
 
 def double_slash_legal(filename):
     if is_double_slash(filename):
-        with open(filename, "rw") as f:
+        with open(filename, "r+b") as f:
             last_pos = f.tell()
             line1 = f.readline()
             if "// Copyright" not in line1:
@@ -113,15 +114,13 @@ def double_slash_legal(filename):
 
 def hash_legal(filename):
     if is_script_file(filename):
-        with open(filename, "rw") as f:
+        with open(filename, "r+b") as f:
             last_pos = f.tell()
             line1 = f.readline()
-            if '#' == line1[0]:
-                # read the rest of the contents
-                content = f.read()
+            content = f.read()
+            if "# Copyright" not in content:
                 f.seek(0)
-                f.write(line1)
-                f.write("\n")
+                f.write("%s\n" % line1)
                 f.write(get_legal_header(filename))
                 f.write(content[1:])
 
@@ -129,6 +128,15 @@ def hash_legal(filename):
 def insert_legal(filename):
     double_slash_legal(filename)
     hash_legal(filename)
+
+
+def cpplint_process(cmd, filename):
+    """Has to have a special process since it just
+   prints sutff to stdout willy nilly"""
+    lint = subprocess.check_output("%s %s" % (cmd, filename), shell=True)
+    if "Total errors found: 0" not in lint:
+        logger.info("Error: %s" % lint)
+        sys.exit(1)
 
 
 def main():
@@ -143,18 +151,19 @@ def main():
 
     for f in files:
         f = "%s/%s" % (root, f)
-        logger.info("Formatting file: %s" % f)
+        logger.debug("Formatting file: %s" % f)
         sys.stdout.write(".")
         insert_legal(f)
         try:
             if is_clang_fmt_file(f):
                 run_subprocess("%s -i %s" % (clang_fmt, f))
-                run_subprocess("%s --verbose=5 --counting=detailed %s" %
-                               (cpplint, f))
+                cpplint_process("%s --verbose=5 --counting=detailed" % cpplint,
+                                f)
         except Exception as e:
             logger.exception("Error %s, file: %s" % (e, f))
             sys.exit(1)
 
+    sys.stdout.write("\nDone\n")
 
 if __name__ == '__main__':
     main()
