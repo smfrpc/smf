3bab45cbc47d7508339f9480ae1178a8ff1bbc48
Alexander Gallego <gallego.alexx@gmail.com>
2018-02-25 19:32:28 -0500

    Issue: #205 - Add friendlier errors on missing files (#207)

--- src/rpc/smf_gen/main.cc
+++ src/rpc/smf_gen/main.cc
@@ -21,35 +21,43 @@ main(int argc, char **argv, char **env) {
   google::InstallFailureSignalHandler();
   google::InitGoogleLogging(argv[0]);
 
-  LOG_IF(FATAL, FLAGS_filename.empty()) << "No filename to parse";
-  LOG_IF(FATAL, !boost::filesystem::exists(FLAGS_filename))
-    << "File does not exists";
-
+  if (FLAGS_filename.empty()) {
+    LOG(ERROR) << "No filename to parse";
+    std::exit(1);
+  }
+  if (!boost::filesystem::exists(FLAGS_filename)) {
+    LOG(ERROR) << " ` " << FLAGS_filename
+               << " ' - does not exists or could not be found";
+    std::exit(1);
+  }
   VLOG(1) << "Parsing file: " << FLAGS_filename
           << ", Include dirs: " << FLAGS_include_dirs;
-
   // generate code!
   flatbuffers::IDLOptions opts;
   flatbuffers::Parser     parser(opts);
   std::string             contents;
-  LOG_IF(FATAL, !flatbuffers::LoadFile(FLAGS_filename.c_str(), true, &contents))
-    << "Could not LOAD file: " << FLAGS_filename;
-
+  if (!flatbuffers::LoadFile(FLAGS_filename.c_str(), true, &contents)) {
+    LOG(ERROR) << "Could not load file: " << FLAGS_filename;
+    std::exit(1);
+  }
   VLOG(1) << FLAGS_filename << " loaded.";
-
+  // flatbuffers uses an ugly C-like API unfort.
+  //
   std::vector<const char *> include_directories;
   auto local_include_directory = flatbuffers::StripFileName(FLAGS_filename);
   include_directories.push_back(local_include_directory.c_str());
   include_directories.push_back(nullptr);
-
-  LOG_IF(FATAL, !parser.Parse(contents.c_str(), &include_directories[0],
-                              FLAGS_filename.c_str()))
-    << "Could not PARSE file: " << parser.error_;
+  if (!parser.Parse(contents.c_str(), &include_directories[0],
+                    FLAGS_filename.c_str())) {
+    LOG(ERROR) << "Could not PARSE file: " << parser.error_;
+    std::exit(1);
+  }
 
   VLOG(1) << "File `" << FLAGS_filename << "` parsed. Generating";
 
-  LOG_IF(FATAL, !smf_gen::generate(parser,
-                                   flatbuffers::StripExtension(FLAGS_filename)))
-    << "Could not generate file";
+  if (!smf_gen::generate(parser, flatbuffers::StripExtension(FLAGS_filename))) {
+    LOG(ERROR) << "Could not generate file";
+    std::exit(1);
+  }
   parser.MarkGenerated();
 }
