fd4ae7433915d6fc5acb192a7f82e9fc89862946
Noah Watkins <noahwatkins@gmail.com>
2018-02-28 13:26:10 -0800

    More submod fixes (#212)
    * build: namespace protect smf build options
    
    e.g. SMF_ENABLE_xxx_TESTS
    
    Signed-off-by: Noah Watkins <noahwatkins@gmail.com>
    
    * cmake: disable building unused bits
    
    Signed-off-by: Noah Watkins <noahwatkins@gmail.com>
    
    * cmake: build only the rpc smf library
    
    this also disables many extra bits that are built in sub-module projects
    (e.g. tests and demos). and by only building the rpc smf library,
    avoiding the intermediate libraries should help build times by avoiding
    extra linking steps.
    
    Signed-off-by: Noah Watkins <noahwatkins@gmail.com>

--- CMake/tests.cmake
+++ CMake/tests.cmake
@@ -1,16 +1,13 @@
 include(CMakeParseArguments)
 enable_testing()
-option(ENABLE_INTEGRATION_TESTS "control if integrations are bulit and ran" ON)
-option(ENABLE_UNIT_TESTS "control if unit tests are bulit and ran" ON)
-option(ENABLE_BENCHMARK_TESTS "control if benchmarks are bulit and ran" OFF)
 set(INTEGRATION_TESTS "")
 set(UNIT_TESTS "")
 set(BENCHMARK_TESTS "")
 set(TEST_RUNNER ${PROJECT_SOURCE_DIR}/src/test_runner.py)
 
-message(STATUS "ENABLE_INTEGRATION_TESTS=${ENABLE_INTEGRATION_TESTS}")
-message(STATUS "ENABLE_UNIT_TESTS=${ENABLE_UNIT_TESTS}")
-message(STATUS "ENABLE_BENCHMARK_TESTS=${ENABLE_BENCHMARK_TESTS}")
+message(STATUS "SMF_ENABLE_INTEGRATION_TESTS=${SMF_ENABLE_INTEGRATION_TESTS}")
+message(STATUS "SMF_ENABLE_UNIT_TESTS=${SMF_ENABLE_UNIT_TESTS}")
+message(STATUS "SMF_ENABLE_BENCHMARK_TESTS=${SMF_ENABLE_BENCHMARK_TESTS}")
 
 function (smf_test)
   set(options INTEGRATION_TEST UNIT_TEST BENCHMARK_TEST)
@@ -18,7 +15,7 @@ function (smf_test)
   set(multiValueArgs SOURCES LIBRARIES)
   cmake_parse_arguments(SMF_TEST "${options}" "${oneValueArgs}" "${multiValueArgs}" ${ARGN})
 
-  if(SMF_TEST_INTEGRATION_TEST AND ENABLE_INTEGRATION_TESTS)
+  if(SMF_TEST_INTEGRATION_TEST AND SMF_ENABLE_INTEGRATION_TESTS)
     set(SMF_TEST_BINARY_NAME "smf_${SMF_TEST_BINARY_NAME}_integration_test")
     set(INTEGRATION_TESTS "${INTEGRATION_TESTS} ${SMF_TEST_BINARY_NAME}")
     add_executable(
@@ -36,7 +33,7 @@ function (smf_test)
       --directory ${SMF_TEST_SOURCE_DIRECTORY}
       )
   endif()
-  if(SMF_TEST_UNIT_TEST AND ENABLE_UNIT_TESTS)
+  if(SMF_TEST_UNIT_TEST AND SMF_ENABLE_UNIT_TESTS)
     set(SMF_TEST_BINARY_NAME "smf_${SMF_TEST_BINARY_NAME}_unit_test")
     set(UNIT_TESTS "${UNIT_TESTS} ${SMF_TEST_BINARY_NAME}")
     add_executable(
@@ -53,7 +50,7 @@ function (smf_test)
       --directory ${SMF_TEST_SOURCE_DIRECTORY}
       )
   endif()
-  if(SMF_TEST_BENCHMARK_TEST AND ENABLE_BENCHMARK_TESTS)
+  if(SMF_TEST_BENCHMARK_TEST AND SMF_ENABLE_BENCHMARK_TESTS)
     set(SMF_TEST_BINARY_NAME "smf_${SMF_TEST_BINARY_NAME}_benchmark_test")
     set(BENCHMARK_TESTS "${BENCHMARK_TESTS} ${SMF_TEST_BINARY_NAME}")
     add_executable(
