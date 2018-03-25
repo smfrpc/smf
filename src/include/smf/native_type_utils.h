0cfed7228b71a1b01b9234f885ed8125c13fad97
Alexander Gallego <gallego.alexx@gmail.com>
2018-03-25 16:27:39 -0400

    fbs: always posix_memalign w/ flatbuffers NativeTable types
    Otherwise you get allocation missalignment faults.

--- src/include/smf/native_type_utils.h
+++ src/include/smf/native_type_utils.h
@@ -20,7 +20,20 @@ seastar::temporary_buffer<char> native_table_as_buffer(
   static thread_local flatbuffers::FlatBufferBuilder builder;
   builder.Clear();
   builder.Finish(RootType::Pack(builder, &t, nullptr));
-  seastar::temporary_buffer<char> retval(builder.GetSize());
+  // always allocate to the largest member 8-bytes
+  void *ret = nullptr;
+  auto r = posix_memalign(&ret, 8, builder.GetSize());
+  if (r == ENOMEM) {
+    throw std::bad_alloc();
+  } else if (r == EINVAL) {
+    throw std::runtime_error(seastar::sprint(
+      "Invalid alignment of %d; allocating %d bytes", 8, builder.GetSize()));
+  }
+  DLOG_THROW_IF(r != 0,
+    "ERRNO: {}, Bad aligned allocation of {} with alignment: {}", r,
+    builder.GetSize(), 8);
+  seastar::temporary_buffer<char> retval(
+    (char *)ret, builder.GetSize(), seastar::make_free_deleter(ret));
   std::memcpy(retval.get_write(),
     reinterpret_cast<const char *>(builder.GetBufferPointer()), retval.size());
   return std::move(retval);
