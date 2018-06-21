841bdd1dbc09fa56ba899e9782edce7fbba62d12
Alexander Gallego <gallego.alexx@gmail.com>
2018-06-20 21:02:51 -0400

    build: update maintainers build system to one script

--- tools/one.sh
+++ tools/one.sh
@@ -10,4 +10,4 @@ THIS_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 name="smf/ci_base_${IMAGE/:/_}"
 docker build -t ${name} --build-arg BASE=${IMAGE} -f ${THIS_DIR}/base/Dockerfile .
-docker run --rm -e USE_NINJA ${name} /src/smf/ci/run.sh
+docker run --rm -e USE_NINJA ${name} /src/smf/tools/run.sh
