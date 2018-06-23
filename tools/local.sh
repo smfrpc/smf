15a52433c3181b65d12299c9f0af2f5cfb605acb
Alexander Gallego <gallego.alexx@gmail.com>
2018-06-22 23:45:34 -0400

    build: fix working directory for executing commands in Dockerfile

--- tools/local.sh
+++ tools/local.sh
@@ -7,11 +7,13 @@ set -e
 set -x
 
 THIS_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-
+ROOT=$(git rev-parse --show-toplevel)
 IMAGES="
 fedora:27
 "
-
+# Always has to be launched from root
+# Docker depends on it
+cd $ROOT
 for img in ${IMAGES}; do
   if [[ ${img} != -* ]]; then
     IMAGE=${img} ${THIS_DIR}/one.sh
--- tools/one.sh
+++ tools/one.sh
@@ -7,7 +7,10 @@ set -e
 set -x
 export DEBIAN_FRONTEND=noninteractive
 THIS_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-
+ROOT=$(git rev-parse --show-toplevel)
+# Always has to be launched from root
+# Docker depends on it
+cd $ROOT
 name="smf/ci_base_${IMAGE/:/_}"
 docker build -t ${name} --build-arg BASE=${IMAGE} -f ${THIS_DIR}/base/Dockerfile .
-docker run --rm ${name} /src/smf/tools/build.sh -dt
+docker run --rm ${name} -w /smf/ ./tools/build.sh -dt
