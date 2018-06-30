9432c1a5aa35eff1f97e8562406ae3ea8b8a26fd
Alexander Gallego <gallego.alexx@gmail.com>
2018-06-30 15:11:28 -0400

    flatbuffers: special case big buffer copies vs local buffers
    flatbuffers: keep stack cache of heap ptr
    
    Prior to this change our fbs buffer would be the
    std::max(all_buffers_serialized) which would only grow and never shrink.
    
    This patch addresses that bug. In addition, we performed several
    micro benchmarks to try and stress small buffers and large buffers and
    make sure that our performance was special cased for both.
    
    Turns out that if you have small buffers, it is cheaper to memcpy
    the serialized flatbuffers bulder into a perfect size destination buffer.
    
    However, if the buffers are large we need to ensure we reset the memory
    to the default of 1024 bytes.
    
    As measured, 8 micro benchmarks this patch has:
    
    1) No worse performance than currently for small buffers
    2) No memory leaks - vs - the old way of taking the max
    3) Improved performance on large buffers by not copying them

--- src/include/smf/native_type_utils.h
+++ src/include/smf/native_type_utils.h
@@ -11,33 +11,61 @@
 
 namespace smf {
 /// \brief converts a flatbuffers::NativeTableType into a buffer.
-/// makes 2 copies. First the table gets copied into a flatbuffer builder
-/// then it gets memcpy into a seastar returned value
+/// See full benchmarks/comparisons here:
+/// https://github.com/senior7515/smf/pull/259
+///
+/// Makes 2 copies in the worst case. See PR #259
+/// If the buffer that we encode is large, we generate a new fbs::builder
+/// to keep memory usage predicatable.
+///
 ///
 template <typename RootType>
 SMF_CONCEPT(requires FlatBuffersNativeTable<RootType>)
 seastar::temporary_buffer<char> native_table_as_buffer(
   const typename RootType::NativeTableType &t) {
-  static thread_local flatbuffers::FlatBufferBuilder builder;
-  builder.Clear();
-  builder.Finish(RootType::Pack(builder, &t, nullptr));
+  static thread_local auto builder =
+    std::make_unique<flatbuffers::FlatBufferBuilder>();
+
+  auto &bdr = *builder.get();
+  bdr.Clear();
+  bdr.Finish(RootType::Pack(bdr, &t, nullptr));
+
+  if (SMF_UNLIKELY(bdr.GetSize() > 2048)) {
+    auto mem = bdr.Release();
+    auto ptr = reinterpret_cast<char *>(mem.data());
+    auto sz = mem.size();
+
+    // fix the original builder
+    builder = std::make_unique<flatbuffers::FlatBufferBuilder>();
+
+    return seastar::temporary_buffer<char>(
+      ptr, sz, seastar::make_object_deleter(std::move(mem)));
+  }
+
+
   // always allocate to the largest member 8-bytes
   void *ret = nullptr;
-  auto r = posix_memalign(&ret, 8, builder.GetSize());
+  auto r = posix_memalign(&ret, 8, bdr.GetSize());
   if (r == ENOMEM) {
     throw std::bad_alloc();
   } else if (r == EINVAL) {
     throw std::runtime_error(seastar::sprint(
-      "Invalid alignment of %d; allocating %d bytes", 8, builder.GetSize()));
+      "Invalid alignment of %d; allocating %d bytes", 8, bdr.GetSize()));
   }
   DLOG_THROW_IF(r != 0,
     "ERRNO: {}, Bad aligned allocation of {} with alignment: {}", r,
-    builder.GetSize(), 8);
-  seastar::temporary_buffer<char> retval(
-    (char *)ret, builder.GetSize(), seastar::make_free_deleter(ret));
+    bdr.GetSize(), 8);
+  seastar::temporary_buffer<char> retval(reinterpret_cast<char *>(ret),
+    bdr.GetSize(), seastar::make_free_deleter(ret));
   std::memcpy(retval.get_write(),
-    reinterpret_cast<const char *>(builder.GetBufferPointer()), retval.size());
+    reinterpret_cast<const char *>(bdr.GetBufferPointer()), retval.size());
   return std::move(retval);
 }
 
 }  // namespace smf
+
+// Reset()
+/*
+  buf.Reset() -> deallocs
+  buf.reserve_size(1024) - reallocs?
+ */
