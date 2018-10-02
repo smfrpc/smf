edb3080cdd6f66a6f58f387ff1c8c3f868069b5a
Alexander Gallego <gallego.alexx@gmail.com>
2018-10-02 13:07:14 -0400

    smf::human_bytes: ostream operator take by value

--- src/include/smf/human_bytes.h
+++ src/include/smf/human_bytes.h
@@ -10,24 +10,28 @@ namespace smf {
 ///
 struct human_bytes {
   explicit human_bytes(double d) : data(d) {}
-  const double data;
+  double data;
 };
+
+// take by value
 inline std::ostream &
-operator<<(std::ostream &o, const ::smf::human_bytes &h) {
+operator<<(std::ostream &o, smf::human_bytes h) {
   auto const orig_precision = o.precision();
   o << std::fixed << std::setprecision(3);
-  auto t = h.data;
-  if (t < 1024) {
-    if (t < 1) { t = 0; }
-    o << t << " bytes";
-  } else if ((t /= static_cast<double>(1024)) < static_cast<double>(1000)) {
-    o << t << " KB";
-  } else if ((t /= static_cast<double>(1024)) < static_cast<double>(1000)) {
-    o << t << " MB";
-  } else if ((t /= static_cast<double>(1024)) < static_cast<double>(1000)) {
-    o << t << " GB";
+  if (h.data < 1024) {
+    if (h.data < 1) { h.data = 0; }
+    o << h.data << " bytes";
+  } else if ((h.data /= static_cast<double>(1024)) <
+             static_cast<double>(1000)) {
+    o << h.data << " KB";
+  } else if ((h.data /= static_cast<double>(1024)) <
+             static_cast<double>(1000)) {
+    o << h.data << " MB";
+  } else if ((h.data /= static_cast<double>(1024)) <
+             static_cast<double>(1000)) {
+    o << h.data << " GB";
   } else {
-    o << t << " TB";
+    o << h.data << " TB";
   }
   // reset to original
   o << std::setprecision(orig_precision);
