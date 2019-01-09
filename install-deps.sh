0faf5c21342e87984b9f5a29dc0c6e190738cd0f
Alexander Gallego <gallego.alexx@gmail.com>
2019-01-09 15:56:43 -0500

    travis: fix debian software properties

--- install-deps.sh
+++ install-deps.sh
@@ -5,86 +5,89 @@
 
 set -e
 
+. /etc/os-release
+
 function debs() {
-  if [ -n "${USE_CLANG}" ]; then
-    extra=clang
-  fi
-  if [ ! -f /usr/bin/add-apt-repository ]; then
-      apt-get -y install software-properties-common
-  fi
-  add-apt-repository -y ppa:ubuntu-toolchain-r/test
-  apt-get update -y
-  apt-get install -y \
-    pkg-config \
-    build-essential \
-    g++ \
-    gcc \
-    make \
-    cmake \
-    libaio-dev \
-    libcrypto++-dev \
-    xfslibs-dev \
-    libunwind-dev \
-    systemtap-sdt-dev \
-    libsctp-dev \
-    libxml2-dev \
-    libpciaccess-dev \
-    ninja-build \
-    libtool \
-    doxygen \
-    stow \
-    python ${extra}
+    if [ -n "${USE_CLANG}" ]; then
+        extra=clang
+    fi
+    if [ "$ID" = "ubuntu" ]; then
+        if [ ! -f /usr/bin/add-apt-repository ]; then
+            apt-get -y install software-properties-common
+        fi
+        add-apt-repository -y ppa:ubuntu-toolchain-r/test
+    fi
+    apt-get update -y
+    apt-get install -y \
+            pkg-config \
+            build-essential \
+            g++ \
+            gcc \
+            make \
+            cmake \
+            libaio-dev \
+            libcrypto++-dev \
+            xfslibs-dev \
+            libunwind-dev \
+            systemtap-sdt-dev \
+            libsctp-dev \
+            libxml2-dev \
+            libpciaccess-dev \
+            ninja-build \
+            libtool \
+            doxygen \
+            stow \
+            python ${extra}
 }
 
 function rpms() {
-  yumdnf="yum"
-  if command -v dnf > /dev/null; then
-    yumdnf="dnf"
-  fi
+    yumdnf="yum"
+    if command -v dnf > /dev/null; then
+        yumdnf="dnf"
+    fi
 
-  ${yumdnf} install -y redhat-lsb-core
-  case $(lsb_release -si) in
-    CentOS)
-      MAJOR_VERSION=$(lsb_release -rs | cut -f1 -d.)
-      $SUDO yum-config-manager --add-repo https://dl.fedoraproject.org/pub/epel/$MAJOR_VERSION/x86_64/
-      $SUDO yum install --nogpgcheck -y epel-release
-      $SUDO rpm --import /etc/pki/rpm-gpg/RPM-GPG-KEY-EPEL-$MAJOR_VERSION
-      $SUDO rm -f /etc/yum.repos.d/dl.fedoraproject.org*
-      ;;
-  esac
+    ${yumdnf} install -y redhat-lsb-core
+    case $(lsb_release -si) in
+        CentOS)
+            MAJOR_VERSION=$(lsb_release -rs | cut -f1 -d.)
+            $SUDO yum-config-manager --add-repo https://dl.fedoraproject.org/pub/epel/$MAJOR_VERSION/x86_64/
+            $SUDO yum install --nogpgcheck -y epel-release
+            $SUDO rpm --import /etc/pki/rpm-gpg/RPM-GPG-KEY-EPEL-$MAJOR_VERSION
+            $SUDO rm -f /etc/yum.repos.d/dl.fedoraproject.org*
+            ;;
+    esac
 
-  if [ -n "${USE_CLANG}" ]; then
-    extra=clang
-  fi
-  ${yumdnf} install -y \
-    cmake \
-    gcc-c++ \
-    make \
-    libpciaccess-devel \
-    libaio-devel \
-    libunwind-devel \
-    libxml2-devel \
-    xfsprogs-devel \
-    systemtap-sdt-devel \
-    lksctp-tools-devel \
-    ninja-build \
-    doxygen \
-    stow \
-    python ${extra}
+    if [ -n "${USE_CLANG}" ]; then
+        extra=clang
+    fi
+    ${yumdnf} install -y \
+              cmake \
+              gcc-c++ \
+              make \
+              libpciaccess-devel \
+              libaio-devel \
+              libunwind-devel \
+              libxml2-devel \
+              xfsprogs-devel \
+              systemtap-sdt-devel \
+              lksctp-tools-devel \
+              ninja-build \
+              doxygen \
+              stow \
+              python ${extra}
 }
 
-source /etc/os-release
 case $ID in
-  debian|ubuntu|linuxmint)
-    debs
-    ;;
+    debian|ubuntu|linuxmint)
+        debs
+        ;;
 
-  centos|fedora)
-    rpms
-    ;;
+    centos|fedora)
+        rpms
+        ;;
 
-  *)
-    echo "$ID not supported. Install dependencies manually."
-    exit 1
-    ;;
+    *)
+        echo "$ID not supported. Install dependencies manually."
+        exit 1
+        ;;
 esac
