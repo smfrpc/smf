97bc729bc3b70c4327a1b795719ecb9d7b60656e
Alexander Gallego <gallego.alexx@gmail.com>
2019-02-21 15:29:31 -0500

    wip: ldfi cannot exist as in in the RPC.

--- src/core/failure_injector_recv_filter.cc
+++ src/core/failure_injector_recv_filter.cc
@@ -9,6 +9,7 @@ seastar::future<rpc_recv_context>
 failure_injector_recv_filter::operator()(rpc_recv_context &&ctx) {
   if (ctx.header.bitflags() &
       rpc::header_bitflags::header_bitflags_has_dynamic_headers) {
+    // rpc::failure_spec spec;
     // TODO(agallego) impl ldfi_fspec
   }
   return seastar::make_ready_future<rpc_recv_context>(std::move(ctx));
