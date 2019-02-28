5332d24c858c5724db2eb5cb18f5601cf904e6b1
Alexander Gallego <gallego.alexx@gmail.com>
2019-02-28 18:57:24 -0500

    build: fix ci dependency manager

--- ci/test.sh
+++ ci/test.sh
@@ -31,7 +31,7 @@ trap "rm -rf ${build_dir}" EXIT
 
 (
   cd ${build_dir}
-  ${CMAKE} -G"${BUILD_GENERATOR}" ${proj_dir}
+  ${CMAKE} -G"${BUILD_GENERATOR}" -DSMF_MANAGE_DEPS=ON ${proj_dir}
   ${build_cmd} -j$(nproc)
   ls -l bin
   ldd bin/smf_demo_server | grep smf-deps-install
