2f1bf3d73d8796560da37e57ed8578028bed3b0c
Alexander Gallego <alex@vectorized.io>
2019-05-29 07:18:38 -0700

    smf: upgrade to c++17
    Signed-off-by: Alexander Gallego <alex@vectorized.io>
    Message-Id: <20190529141838.23764-1-alex@vectorized.io>

--- src/include/smf/std-compat.h
+++ src/include/smf/std-compat.h
@@ -2,11 +2,6 @@
 //
 #pragma once
 
-#if __cplusplus >= 201703L && __has_include(<optional>) \
-    && __has_include(<string_view>) && __has_include(<variant>)
-#define SEASTAR_USE_STD_OPTIONAL_VARIANT_STRINGVIEW
-#endif
-
 #include <seastar/util/std-compat.hh>
 
 namespace smf {
