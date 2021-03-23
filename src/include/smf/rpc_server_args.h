0ef75a3816458f733c9b6323351341279338a2b5
lumontec <lucamontechiesi@gmail.com>
2021-03-23 01:00:00 -0700

    tls: add server args credentials
    Signed-off-by: lumontec <lucamontechiesi@gmail.com>

--- src/include/smf/rpc_server_args.h
+++ src/include/smf/rpc_server_args.h
@@ -7,6 +7,7 @@
 
 #include <seastar/core/sstring.hh>
 #include <seastar/core/timer.hh>
+#include <seastar/net/tls.hh>
 
 namespace smf {
 enum rpc_server_flags : uint32_t { rpc_server_flags_disable_http_server = 1 };
@@ -20,6 +21,9 @@ struct rpc_server_args {
   ///
   uint32_t flags = 0;
 
+  /// \ brief rpc server tls certs and keys
+  ///
+  seastar::shared_ptr<seastar::tls::server_credentials> credentials;
   /// \ brief The default timeout PER connection body. After we parse the
   /// header of the connection we need to make sure that we at some point
   /// receive some bytes or expire the connection.
